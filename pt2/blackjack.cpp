5bf202eefc4f64530ac3fcb71e64a51e4e5774d1
delamorte <alabasterclock@gmail.com>
2021-07-15 14:17:26 +0300

    added task 2.5, put blackjack classes into a separate file

---
+++ pt2/blackjack.cpp
@@ -0,0 +1,127 @@
+#include <iostream>
+#include <cstdint>
+#include <ctime>
+#include <vector>
+
+using namespace std;
+
+namespace BlackJack
+{
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+enum cardsuit {hearts, spades, diamonds, clubs};
+enum cardrank {ace=1, two=2, three=3, four=4, five=5, six=6, seven=7, eight=8, nine=9, ten=10, jack=10, king=10, queen=10};
+
+class Card
+{
+    private:
+        cardrank rank;
+        cardsuit suit;
+        bool istapped;
+    public:
+        Card(cardrank nrank, cardsuit nsuit) : rank(nrank), suit(nsuit), istapped(false)
+        {
+        }
+
+        ~Card()
+        {
+        }
+
+        void Flip() 
+        {
+            istapped = !istapped;
+        }
+
+        int Getvalue() const
+        {
+            return rank;
+        }
+};
+
+class Hand
+{
+    private:
+        vector<Card*> deck;
+    public:
+        Hand()
+        {
+        }
+
+        ~Hand()
+        {
+        }
+
+        void Add(Card *newcard)
+        {
+            deck.push_back(newcard);
+        }
+
+        void Clear()
+        {
+            vector<Card*>::iterator iter;
+            for (iter = deck.begin(); iter !=deck.end(); iter++)
+            {
+                delete *iter;
+                *iter = 0;
+            }
+
+            deck.clear();
+        }
+
+        int Count()
+        {
+           return deck.size();
+        }
+
+        int getValue() const                    // So, I assume that we only need 11 point Ace for cases where extra points would let us win (ie closer to 21)
+        {
+            bool hasAce = false;                        // doesnt matter how many aces we have, since even 2 aces is 22 points.
+            int tempvalue = 0 ;
+            for (int i = 0; i < deck.size(); i++)
+            {
+                if (deck[i]->Getvalue() == 1)
+                    hasAce = true;
+                tempvalue += deck[i]->Getvalue();
+            }
+            if ((hasAce) && tempvalue < 12)             // only adding for safe situations
+                tempvalue += 10;                        
+
+            return tempvalue;
+        }
+};
+
+class GenericPlayer : public Hand
+{
+    private:
+        string name;
+    public: 
+        GenericPlayer(string n) : Hand(), name(n)
+        {
+        }
+
+        ~GenericPlayer()
+        {
+        }
+
+        virtual bool IsHitting() const = 0;
+
+        bool IsBusted() const
+        {
+            if (getValue() > 21)
+                return true;
+            else
+                return false;
+        }
+
+        void Bust() const
+        {
+            cout << name << " has lost with " << getValue() << " points";
+        }
+
+        string getName() const
+        {
+            return name;
+        }
+};
+
+}
---
+++ pt2/t5.cpp
@@ -0,0 +1,103 @@
+#include <iostream>
+#include <cstdint>
+#include <ctime>
+#include <vector>
+#include "blackjack.cpp"
+
+using namespace std;
+using namespace BlackJack;
+
+template <typename T>
+class Pair1
+{
+    private:
+        T firstval;
+        T secondval;
+    public:
+        Pair1(T fv, T tv) : firstval(fv), secondval(tv)
+        {
+        }
+
+        ~Pair1()
+        {
+        }
+
+        T first() const 
+        {
+            return firstval;
+        }
+
+        T second() const 
+        {
+            return secondval;
+        }
+};
+
+template <typename T, typename N>
+class Pair
+{
+    private:
+        T firstval;
+        N secondval;
+    public:
+        Pair(T fv, N tv) : firstval(fv), secondval(tv)
+        {
+        }
+
+        ~Pair()
+        {
+        }
+
+        T first() const
+        {
+            return firstval;
+        }
+
+        N second() const
+        {
+            return secondval;
+        }
+};
+
+template<typename T>
+class StringValuePair : public Pair<string, T>
+{
+    private:
+        string firstval;
+        T secondval;
+    public:
+        StringValuePair(string fv, T tv) : Pair<string, int> (fv, tv)
+        {
+        }
+
+        ~StringValuePair()
+        {
+        }
+};
+
+
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+int main()
+{
+//========================================================================================================================
+cout << "Task 1: " << endl;
+Pair1<int> p1(6,9);
+cout << "Pair: " << p1.first() << " " << p1.second() << endl;
+
+const Pair1<double> p2(3.4, 7.8);
+cout << "Pair: " << p2.first() << " " << p2.second() << endl;
+//========================================================================================================================
+cout << endl << "Task 2: " << endl;
+Pair<int, double> pvar1(6, 7.8);
+cout << "Pair: " << pvar1.first() << " " << pvar1.second() << endl;
+
+Pair<double, int> pvar2(3.4, 5);
+cout << "Pair: " << pvar2.first() << " " << pvar2.second() << endl;
+
+//========================================================================================================================
+cout << endl << "Task 3: " << endl;
+StringValuePair<int> svp("Amazing", 7);
+cout << "Pair: "  << svp.first() << ' ' << svp.second() << endl;
+
+    return 0;
+}
